dad380403f5f8ffff1cda9f3ca4347cbd341c78a
TheoDelaunoy <59996960+TheoDelaunoy@users.noreply.github.com>
2023-07-19 15:42:59 +0200

    Add files via upload

---
+++ Machina/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Xamarin.Forms;
+
+namespace Machina
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        void StartButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            StartButtonClickedAsync();
+        }
+
+        private async Task StartButtonClickedAsync()
+        {
+            await CrossMedia.Current.Initialize();
+            if(!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                DisplayAlert("Erreur", ": no camera available", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if(file == null)
+            {
+                return;
+            }
+
+            await Navigation.PushAsync(new ScannerPage(file));
+        }
+    }
+}
+
---
+++ Machina/ScannerPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace Machina
+{	
+	public partial class ScannerPage : ContentPage
+	{	
+		public ScannerPage (MediaFile file)
+		{
+			InitializeComponent ();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            faceImage.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStreamWithImageRotatedForExternalStorage();
+                return stream;
+            });
+            
+        }
+
+        void ContinueButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
+
